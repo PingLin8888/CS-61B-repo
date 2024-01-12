b75b81818a00221f95157c703d37e134c5908da3
Phoebe <limphoebe8@gmail.com>
2024-01-12 12:51:58 +0000

    Passed the bomb tests. Debugging.

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -11,13 +11,22 @@ public class BombMain {
         // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(IntList.of(0,9,3,0,8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            String repeatedString = repeatString("Figure this out. I wonder -81201430 the phases are defined...", 10000);
+            b.phase2(repeatedString);
         }
+    }
+
+    private static String repeatString(String s, int times) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < times; i++) {
+            result.append(s);
+        }
+        return result.toString();
     }
 }
