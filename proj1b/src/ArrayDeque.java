130ac27a31fdab5af95fd83ba207caf733374085
Phoebe <limphoebe8@gmail.com>
2024-01-16 22:48:48 +0000

    Implemented addFirst, addLast and resizeUp. Passed relative tests.

---
+++ proj1b/src/ArrayDeque.java
@@ -0,0 +1,102 @@
+import java.util.List;
+
+public class ArrayDeque<T> implements Deque<T> {
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    T[] items;
+
+    public ArrayDeque() {
+        items= (T[]) new Object[8];
+        size = 0;
+        nextFirst = 3;
+        nextLast = 4;
+    }
+
+    @Override
+    public void addFirst(Object x) {
+        if (size == items.length) {
+            resizeUp(2 * size);
+        }
+        items[nextFirst] = (T) x;
+        updateNextFirst(nextFirst);
+        size++;
+    }
+
+    private void updateNextFirst(int nextFirst) {
+        if (nextFirst == 0) {
+            this.nextFirst = items.length - 1;
+        }
+        else {
+            this.nextFirst--;
+        }
+    }
+
+    private void resizeUp(int capacity) {
+        T[] temp = (T[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            if (nextFirst + 1 + i >= size) {
+                temp[i] = items[nextFirst + 1 + i -size];
+            } else {
+                temp[i] = items[nextFirst + 1 + i];
+            }
+        }
+        items = temp;
+        nextFirst = items.length - 1;
+        nextLast = size;
+    }
+
+    @Override
+    public void addLast(Object x) {
+        if (size == items.length) {
+            resizeUp(2 * size);
+        }
+        items[nextLast] = (T) x;
+        updateNextLast(nextLast);
+        size++;
+    }
+
+    private void updateNextLast(int nextLast) {
+        if (nextLast == items.length - 1) {
+            this.nextLast = 0;
+        }
+        else {
+            this.nextLast++;
+        }
+    }
+
+    @Override
+    public List toList() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -10,14 +10,72 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
 
-    // @Test
-    // @DisplayName("ArrayDeque has no fields besides backing array and primitives")
-    // void noNonTrivialFields() {
-    //     List<Field> badFields = Reflection.getFields(ArrayDeque.class)
-    //             .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-    //             .toList();
+     @Test
+     @DisplayName("ArrayDeque has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
 
-    //     assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-    // }
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+
+    @Test
+    /** In this test, we have three different assert statements that verify that addFirst works correctly. */
+    public void addFirstTestBasic() {
+        Deque<String> lld1 = new ArrayDeque<>();
+
+        lld1.addFirst("back"); // after this call we expect: ["back"]
+        lld1.addFirst("middle"); // after this call we expect: ["middle", "back"]
+        lld1.addFirst("front"); // after this call we expect: ["front", "middle", "back"]
+        lld1.addFirst("apple");
+        lld1.addFirst("orange");
+        lld1.addFirst("banana");
+        lld1.addFirst("cherry");
+        lld1.addFirst("blueberry");
+        lld1.addFirst("blackberry");
+//        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+
+    }
+
+    @Test
+    /** In this test, we use only one assertThat statement. IMO this test is just as good as addFirstTestBasic.
+     *  In other words, the tedious work of adding the extra assertThat statements isn't worth it. */
+    public void addLastTestBasic() {
+        Deque<String> lld1 = new ArrayDeque<>();
+
+        lld1.addLast("front"); // after this call we expect: ["front"]
+        lld1.addLast("middle"); // after this call we expect: ["front", "middle"]
+        lld1.addLast("back"); // after this call we expect: ["front", "middle", "back"]
+        lld1.addLast("apple");
+        lld1.addLast("orange");
+        lld1.addLast("banana");
+        lld1.addLast("cherry");
+        lld1.addLast("blueberry");
+        lld1.addLast("blackberry");
+
+//        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+
+    @Test
+    /** This test performs interspersed addFirst and addLast calls. */
+    public void addFirstAndAddLastTest() {
+        Deque<Integer> lld1 = new ArrayDeque<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+        lld1.addLast(0);   // [0]
+        lld1.addLast(1);   // [0, 1]
+        lld1.addFirst(-1); // [-1, 0, 1]
+        lld1.addLast(2);   // [-1, 0, 1, 2]
+        lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+        lld1.addFirst(-1);
+        lld1.addLast(11);
+        lld1.addLast(22);
+        lld1.addFirst(88);
+        lld1.addLast(-99);
+
+//        assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
+    }
 
 }
