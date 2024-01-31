07ef89129959ddd04424d8ee234d5036b1924012
Ping Lin <limphoebe8@gmail.com>
2024-01-31 22:48:23 +0000

    Finished HistoryHandler. Plot the chart successfully.

--- proj2a/src/main/HistoryHandler.java
+++ proj2a/src/main/HistoryHandler.java
@@ -1,2 +1,34 @@
-package main;public class HistoryHandler {
+package main;
+
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import ngrams.NGramMap;
+import ngrams.TimeSeries;
+import org.knowm.xchart.XYChart;
+import plotting.Plotter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryHandler extends NgordnetQueryHandler {
+    private NGramMap data;
+
+    public HistoryHandler(NGramMap map) {
+        data = map;
+    }
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+        ArrayList<TimeSeries> lts = new ArrayList<>();
+        ArrayList<String> labels = new ArrayList<>();
+        for (String word : words) {
+            labels.add(word);
+            lts.add(data.weightHistory(word, startYear, endYear));
+        }
+        XYChart chart = Plotter.generateTimeSeriesChart(labels, lts);
+        String encodedImage = Plotter.encodeChartAsString(chart);
+        return encodedImage;
+    }
 }
--- proj2a/src/main/Main.java
+++ proj2a/src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import browser.NgordnetServer;
+import ngrams.NGramMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,10 +14,13 @@ public class Main {
         NGramMap ngm = new NGramMap(wordFile, countFile);
 
         */
+        String wordFile = "./data/ngrams/top_14377_words.csv";
+        String countFile = "./data/ngrams/total_counts.csv";
+        NGramMap ngm = new NGramMap(wordFile, countFile);
 
         hns.startUp();
-        hns.register("history", new DummyHistoryHandler());
-        hns.register("historytext", new DummyHistoryTextHandler());
+        hns.register("history", new HistoryHandler(ngm));
+        hns.register("historytext", new HistoryTextHandler(ngm));
 
         System.out.println("Finished server startup! Visit http://localhost:4567/ngordnet_2a.html");
     }
